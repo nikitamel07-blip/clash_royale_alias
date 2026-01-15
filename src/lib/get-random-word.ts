534e75bdab7f893871fc8049219de7ff7fbc004a
nikitamel07-blip <nikitamel07@gmail.com>
2026-01-15 17:51:19 +0200

    Add files via upload

---
+++ src/lib/get-random-word.ts
@@ -0,0 +1,16 @@
+import words from "./top-2000-english-words.json";
+
+const usedWords: string[] = [];
+
+export const getRandomWord = (): string => {
+  const word = words[Math.floor(Math.random() * words.length)];
+
+  // If a word was used during the session - try another word
+  if (usedWords.includes(word)) {
+    return getRandomWord();
+  }
+
+  usedWords.push(word);
+
+  return word;
+};
